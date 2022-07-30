6000cc9151e6220bc84e34b79efbc44e59b556b0
sunliang711 <sunliang711@163.com>
2022-07-30 11:00:52 +0800

    treesitter config;

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,28 +6,41 @@ function M.setup()
         return
     end
 
-    local ft_to_parser = require "nvim-treesitter.parsers".filetype_to_parsername
-    ft_to_parser.motoko = "typescript"
-
     configs.setup({
         -- ensure_installed = "all", -- one of "all" or a list of languages
         -- reference https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-        ensure_installed = { "c", "cpp", "css", "go", "html", "javascript", "json", "jsdoc", "lua", "make", "markdown",
+        ensure_installed = {
+            "c", "cpp",
+            "css", "html",
+            "javascript", "jsdoc",
+
+            "json",
+            "toml",
+            "yaml",
+
+            "lua",
+            "vim",
+            "make",
+            "markdown",
             "regex",
-            "rust", "toml", "vim", "yaml" },
+            "go",
+            "rust",
+        },
         sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
         ignore_install = { "" }, -- List of parsers to ignore installing
         highlight = {
             -- use_languagetree = true,
             enable = true, -- false will disable the whole extension
             -- disable = { "css", "html" }, -- list of language that will be disabled
-            disable = { "css", "markdown" }, -- list of language that will be disabled
             -- additional_vim_regex_highlighting = true,
         },
         autopairs = {
             enable = true,
         },
-        indent = { enable = true, disable = { "python", "css" } },
+        indent = {
+            enable = true,
+            disable = { "css" }
+        },
         context_commentstring = {
             enable = true,
             enable_autocmd = false,
