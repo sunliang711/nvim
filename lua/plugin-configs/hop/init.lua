8bad4022aeda69be3531d3661c6b84928096a22d
sunliang711 <sunliang711@163.com>
2023-07-28 13:13:01 +0800

    hop keymap

--- lua/plugin-configs/hop/init.lua
+++ lua/plugin-configs/hop/init.lua
@@ -16,26 +16,30 @@ function M.setup()
     keymap('n', 'gp', "<cmd>HopPattern<cr>", opts)
 
     -- keymap("n", "]b", "<cmd>bnext<cr>", opts)
-    keymap('n', 'f',
-        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
-        , {})
-    keymap('n', 'F',
-        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
-        , {})
-    keymap('o', 'f',
-        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>"
-        , {})
-    keymap('o', 'F',
-        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>"
-        , {})
 
-    -- maybe wrong config
-    keymap('', 't',
-        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
-        , {})
-    keymap('', 'T',
-        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
-        , {})
+    -- 这个配置如果配合df<char> dt<char>时需要多选一次
+    -- TODO 增加一个可以toggle下面快捷键的方法
+    -- 按一次map，再按一次unmap
+    -- keymap('n', 'f',
+    --     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
+    --     , {})
+    -- keymap('n', 'F',
+    --     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
+    --     , {})
+    -- keymap('o', 'f',
+    --     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>"
+    --     , {})
+    -- keymap('o', 'F',
+    --     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>"
+    --     , {})
+    --
+    -- -- maybe wrong config
+    -- keymap('', 't',
+    --     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
+    --     , {})
+    -- keymap('', 'T',
+    --     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
+    --     , {})
 
     keymap('n', '<leader>e',
         "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
