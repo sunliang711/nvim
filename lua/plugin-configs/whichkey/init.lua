a2bb8a7cc5a5be6ed024482fc1adf4e0c7b5bbeb
sunliang711 <sunliang711@163.com>
2023-11-19 19:40:28 +0800

    which-key: lsp

--- lua/plugin-configs/whichkey/init.lua
+++ lua/plugin-configs/whichkey/init.lua
@@ -8,22 +8,22 @@ function M.setup()
 
     local setup = {
         plugins = {
-            marks = true, -- shows a list of your marks on ' and `
-            registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+            marks = true,         -- shows a list of your marks on ' and `
+            registers = true,     -- shows your registers on " in NORMAL or <C-r> in INSERT mode
             spelling = {
-                enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+                enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
                 suggestions = 20, -- how many suggestions should be shown in the list?
             },
             -- the presets plugin, adds help for a bunch of default keybindings in Neovim
             -- No actual key bindings are created
             presets = {
-                operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-                motions = false, -- adds help for motions
+                operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+                motions = false,      -- adds help for motions
                 text_objects = false, -- help for text objects triggered after entering an operator
-                windows = true, -- default bindings on <c-w>
-                nav = true, -- misc bindings to work with windows
-                z = true, -- bindings for folds, spelling and others prefixed with z
-                g = true, -- bindings for prefixed with g
+                windows = true,       -- default bindings on <c-w>
+                nav = true,           -- misc bindings to work with windows
+                z = true,             -- bindings for folds, spelling and others prefixed with z
+                g = true,             -- bindings for prefixed with g
             },
         },
         -- add operators that will trigger motion and text object completion
@@ -43,24 +43,24 @@ function M.setup()
         },
         popup_mappings = {
             scroll_down = "<c-d>", -- binding to scroll down inside the popup
-            scroll_up = "<c-u>", -- binding to scroll up inside the popup
+            scroll_up = "<c-u>",   -- binding to scroll up inside the popup
         },
         window = {
-            border = "rounded", -- none, single, double, shadow
-            position = "bottom", -- bottom, top
-            margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+            border = "rounded",       -- none, single, double, shadow
+            position = "bottom",      -- bottom, top
+            margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
             padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
             winblend = 0,
         },
         layout = {
-            height = { min = 4, max = 25 }, -- min and max height of the columns
-            width = { min = 20, max = 50 }, -- min and max width of the columns
-            spacing = 3, -- spacing between columns
-            align = "center", -- align columns left, center or right
+            height = { min = 4, max = 25 },                                           -- min and max height of the columns
+            width = { min = 20, max = 50 },                                           -- min and max width of the columns
+            spacing = 3,                                                              -- spacing between columns
+            align = "center",                                                         -- align columns left, center or right
         },
-        ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+        ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
         hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-        show_help = false, -- show help message on the command line when the popup is visible
+        show_help = false,                                                            -- show help message on the command line when the popup is visible
         -- triggers = "auto", -- automatically setup triggers
         -- triggers = {"<leader>"} -- or specify a list manually
         triggers_blacklist = {
@@ -73,21 +73,21 @@ function M.setup()
     }
 
     local opts = {
-        mode = "n", -- NORMAL mode
+        mode = "n",     -- NORMAL mode
         prefix = "<leader>",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     }
 
     local m_opts = {
-        mode = "n", -- NORMAL mode
+        mode = "n",     -- NORMAL mode
         prefix = "m",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     }
 
     local m_mappings = {
@@ -286,38 +286,38 @@ function M.setup()
         -- require("dapui").close()
         -- require("dapui").toggle()
 
-        -- l = {
-        --   name = "LSP",
-        --   a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-        --   d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
-        --   w = {
-        --     "<cmd>Telescope lsp_workspace_diagnostics<cr>",
-        --     "Workspace Diagnostics",
-        --   },
-        --   f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
-        --   F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
-        --   i = { "<cmd>LspInfo<cr>", "Info" },
-        --   I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
-        --   j = {
-        --     "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
-        --     "Next Diagnostic",
-        --   },
-        --   k = {
-        --     "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
-        --     "Prev Diagnostic",
-        --   },
-        --   l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-        --   o = { "<cmd>SymbolsOutline<cr>", "Outline" },
-        --   q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
-        --   r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-        --   R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
-        --   s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-        --   S = {
-        --     "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-        --     "Workspace Symbols",
-        --   },
-        --   t = { '<cmd>lua require("functions").toggle_diagnostics()<cr>', "Toggle Diagnostics" },
-        -- },
+        l = {
+            name = "LSP",
+            a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+            d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
+            w = {
+                "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+                "Workspace Diagnostics",
+            },
+            f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
+            F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
+            i = { "<cmd>LspInfo<cr>", "Info" },
+            I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+            j = {
+                "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
+                "Next Diagnostic",
+            },
+            k = {
+                "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>",
+                "Prev Diagnostic",
+            },
+            l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+            o = { "<cmd>SymbolsOutline<cr>", "Outline" },
+            q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+            r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+            R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
+            s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+            S = {
+                "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+                "Workspace Symbols",
+            },
+            t = { '<cmd>lua require("functions").toggle_diagnostics()<cr>', "Toggle Diagnostics" },
+        },
 
         -- s = {
         --   name = "Surround",
@@ -353,12 +353,12 @@ function M.setup()
     }
 
     local vopts = {
-        mode = "v", -- VISUAL mode
+        mode = "v",     -- VISUAL mode
         prefix = "<leader>",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     }
     local vmappings = {
         ["/"] = { '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', "Comment" },
