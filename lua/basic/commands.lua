b506eb8dfe8b442a1c362d531898a41fffdd263a
sunliang711 <sunliang711@163.com>
2024-01-30 11:16:18 +0800

    custom command: Push,Pull

--- lua/basic/commands.lua
+++ lua/basic/commands.lua
@@ -2,16 +2,21 @@ local M = {}
 
 function M.setup()
     -- format json
-    vim.cmd [[
+    vim.cmd([[
 command! JsonFormat :%!python3 -m json.tool
-]]
+]])
 
     -- save as root
-    vim.cmd [[
+    vim.cmd([[
 command! W :execute ':silent w !sudo tee % >/dev/null' | :edit!
 command! Wq :execute ':silent w !sudo tee % >/dev/null' | :edit! | :quit
-]]
+]])
 
+    -- push (need share installed)
+    vim.cmd([[
+command! -range=% Push <line1>,<line2>!share push
+command! -nargs=1 Pull :r !share pull <args>
+]])
 end
 
 return M
