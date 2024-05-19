b881d53028c64f58edac004b2d73a2f25110ee20
sunliang711 <sunliang711@163.com>
2024-05-19 12:19:30 +0800

    auto-session: telescope

--- lua/plugin-configs/auto-session/init.lua
+++ lua/plugin-configs/auto-session/init.lua
@@ -11,6 +11,20 @@ function M.setup()
         auto_session_enable_last_session = false,
         auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
         auto_session_enabled = true,
+
+        session_lens = {
+            -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+            buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+            load_on_setup = true,
+            theme_conf = { border = true },
+            previewer = false,
+            auto_session_suppress_dirs = { "~/", "~/Workspace", "~/Downloads", "/" },
+        },
+        -- Set mapping for searching a session.
+        -- ⚠️ This will only work if Telescope.nvim is installed
+        vim.keymap.set("n", "<Leader>a", require("auto-session.session-lens").search_session, {
+            noremap = true,
+        }),
     })
 end
 
