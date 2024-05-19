4e6c21fc2506d1d6b1086fbf5e15409fec8eb157
sunliang711 <sunliang711@163.com>
2024-05-19 10:04:12 +0800

    autocmd for golang: set nolist

--- lua/basic/autocmds.lua
+++ lua/basic/autocmds.lua
@@ -3,19 +3,28 @@ local M = {}
 function M.setup()
     vim.api.nvim_create_autocmd({ "BufReadPost" }, {
         callback = function()
-            vim.cmd [[
+            vim.cmd([[
                  if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif
-        ]]
+        ]])
         end,
     })
 
-    vim.cmd [[
-  augroup YankHighlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-  augroup end
-]]
+    vim.cmd([[
+        -- 复制时高亮
+        augroup YankHighlight
+            autocmd!
+            autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+        augroup end
+    ]])
 
+    vim.cmd([[
+        -- go语言设置
+        augroup golang
+            autocmd!
+            -- set list 和 indent-blankline插件冲突，这里取消掉
+            autocmd FileType go set nolist
+        augroup end
+    ]])
 end
 
 return M
