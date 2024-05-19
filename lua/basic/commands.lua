62aab06009d275754437739e75def0fd6c663605
sunliang711 <sunliang711@163.com>
2024-05-19 21:00:09 +0800

    pure unpure

--- lua/basic/commands.lua
+++ lua/basic/commands.lua
@@ -20,11 +20,11 @@ command! -nargs=1 Pull :r !share pull <args>
 
     -- 清除文件内容以外的字符，方便复制(按住option用鼠标选择)
     vim.cmd([[
-    command! Pure :set nonumber | :set norelativenumber | :set nolist
+    command! Pure :set nonumber | :set norelativenumber | :set nolist | :IBLDisable
     ]])
 
     vim.cmd([[
-    command! Unpure :set number | :set relativenumber | :set list
+    command! Unpure :set number | :set relativenumber | :set list | :IBLEnable
     ]])
 end
 
