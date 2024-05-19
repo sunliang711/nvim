f8d06b36006bedbc8e4ffbe1440b7701c230733d
sunliang711 <sunliang711@163.com>
2024-05-19 09:54:46 +0800

    toggle term

--- lua/plugin-configs/toggleterm/init.lua
+++ lua/plugin-configs/toggleterm/init.lua
@@ -16,7 +16,7 @@ function M.setup()
         start_in_insert = true,
         insert_mappings = true,
         persist_size = true,
-        direction = "float",
+        -- direction = "float",
         close_on_exit = true,
         shell = vim.o.shell,
         float_opts = {
@@ -32,14 +32,14 @@ function M.setup()
     function _G.set_terminal_keymaps()
         local opts = { noremap = true }
         -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-        vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-        vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-        vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-        vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-        vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
     end
 
-    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
     local Terminal = require("toggleterm.terminal").Terminal
     local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
@@ -75,8 +75,10 @@ function M.setup()
     local keymap = vim.api.nvim_set_keymap
     local opts = { noremap = true, silent = true }
 
-    keymap("n", "<C-t>", "<cmd>ToggleTerm size=30 direction=float<cr>", opts)
-    keymap("t", "<C-t>", "<cmd>ToggleTerm size=30 direction=float<cr>", opts)
+    -- keymap("n", "<C-t>", "<cmd>ToggleTerm size=30 direction=float<cr>", opts)
+    -- keymap("t", "<C-t>", "<cmd>ToggleTerm size=30 direction=float<cr>", opts)
+    keymap("n", "<C-t>", "<cmd>ToggleTerm size=30 <cr>", opts)
+    keymap("t", "<C-t>", "<cmd>ToggleTerm size=30 <cr>", opts)
 
     -- vim.api.nvim_create_autocmd( { "TermEnter" }, {
     --     pattern = { "term://*toggleterm#*" },
