c9b1e9ca266ab13f8ea785b523789aa7145154b1
sunliang711 <sunliang711@163.com>
2024-05-19 10:08:35 +0800

    comment

--- lua/basic/autocmds.lua
+++ lua/basic/autocmds.lua
@@ -10,18 +10,16 @@ function M.setup()
     })
 
     vim.cmd([[
-        -- 复制时高亮
         augroup YankHighlight
             autocmd!
             autocmd TextYankPost * silent! lua vim.highlight.on_yank()
         augroup end
     ]])
 
+    -- set list 和 indent-blankline插件冲突，这里取消掉
     vim.cmd([[
-        -- go语言设置
         augroup golang
             autocmd!
-            -- set list 和 indent-blankline插件冲突，这里取消掉
             autocmd FileType go set nolist
         augroup end
     ]])
