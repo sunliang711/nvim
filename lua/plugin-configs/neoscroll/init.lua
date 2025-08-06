65c36fec7a74ff5055dd522b7df000bf2cc7bcc2
eagle <sunliang711@163.com>
2025-08-06 15:58:30 +0800

    comment

--- lua/plugin-configs/neoscroll/init.lua
+++ lua/plugin-configs/neoscroll/init.lua
@@ -11,7 +11,7 @@ function M.setup()
         mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
         hide_cursor = true, -- Hide cursor while scrolling
         stop_eof = true, -- Stop at <EOF> when scrolling downwards
-        duration_multiplier = 0.2,   -- Global duration multiplier
+        duration_multiplier = 0.2,   -- Global duration multiplier 越小滚动地越快
         use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
         respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
         cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
