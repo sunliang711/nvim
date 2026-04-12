dc8d7e972b0cb98eb378b0ab90697b615f38a473
sunliang711 <sunliang711@163.com>
2026-04-12 11:03:40 +0800

    File Explorer hot key

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,15 +1,16 @@
 local enabled = PLUGINS.nvimtree == nil or PLUGINS.nvimtree.enabled ~= false
 
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
-    keys = {
-        { "<leader>fe", "<cmd>NvimTreeToggle<cr>", desc = "File Explorer" },
-    },
-    config = function()
-        require("plugin-configs.nvimtree").setup()
-    end,
-    enabled = enabled,
-    cond = enabled,
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	keys = {
+		{ "<leader>fe", "<cmd>NvimTreeToggle<cr>", desc = "File Explorer" },
+		{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "File Explorer" },
+	},
+	config = function()
+		require("plugin-configs.nvimtree").setup()
+	end,
+	enabled = enabled,
+	cond = enabled,
 }
